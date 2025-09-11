b7534969cc9f9648c019c0a53f475204780fdbd1
Aryan-561 <aryanmail561@gmail.com>
2025-09-12 00:15:24 +0530

    refactor: reorganize imports and enhance dotenv configuration

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
+import { dbConnect } from "./dbConnect";
 import { indexTheDocument } from "./prepare";
+import * as dotenv from 'dotenv';
 
-console.log('RAG-PDF');
+dotenv.config({
+    path: '.env'
+})
+
+
 
 const filePath = './test.pdf'; //add your pdf file path here
 indexTheDocument(filePath)
+
+dbConnect()
