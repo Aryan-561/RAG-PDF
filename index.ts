5a208252e5fc6c61e0dc88648d894da3f0a9fd7a
Aryan-561 <aryanmail561@gmail.com>
2025-09-13 17:13:50 +0530

    feat: integrate chat functionality into main execution flow

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import { dbConnect } from "./dbConnect";
+import { chat } from "./chat";
+import { db ,dbConnect } from "./dbConnect";
 import { indexTheDocument } from "./prepare";
 import * as dotenv from 'dotenv';
 
@@ -6,9 +7,23 @@ dotenv.config({
     path: '.env'
 })
 
+export const collection = db.collection("ragpdf");
 
 
-const filePath = './test.pdf'; //add your pdf file path here
-indexTheDocument(filePath)
 
 dbConnect()
+
+async function main(){
+    try {
+        await dbConnect()
+        const filePath = './test.pdf'; //add your pdf file path here
+        await indexTheDocument(filePath) // index the document only once
+        console.log('\n')
+        await chat()
+        
+    } catch (error) {
+        console.error('Error in main:', error);
+    }
+}
+
+main()
